7b090d6cb2161bf4668b55fcedd72505c969e31c
el-bekasto <beksei7@gmail.com>
2023-11-10 14:16:53 +0600

    reformatted urls

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -4,6 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('api.urls')),
-    path('session_auth/', include('session_auth.urls')),
+    path('sessionAuth/', include('session_auth.urls')),
     path('auth/', include('rest_framework.urls')),
 ]
