c554a6ebaba1fc8cc53c51e917b753dcc3c5c83f
el-bekasto <beksei7@gmail.com>
2023-11-14 19:45:38 +0600

    fixed url slash missing

--- backend/session_auth/urls.py
+++ backend/session_auth/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('register', SignupView.as_view(), name='register'),
+    path('register/', SignupView.as_view(), name='register'),
     path('csrfCookie/', GetCSRFTokenView.as_view(), name='csrf_cookie'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
