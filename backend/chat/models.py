72c530fc84b214073752ee4e8e33faa35139cce8
el-bekasto <beksei7@gmail.com>
2023-11-17 20:16:09 +0600

    updated models, made it more logical

--- backend/chat/models.py
+++ backend/chat/models.py
@@ -14,6 +14,25 @@ class User(AbstractUser):
         return f'<chatUser {self.username}>'
 
 
+class Participant(models.Model):
+    MEMBER = 'member'
+    OWNER = 'owner'
+    ADMIN = 'admin'
+    RESTRICTED = 'restricted'
+    LEFT = 'left'
+    BANNED = 'banned'
+    PARTICIPANT_TYPE_CHOICES = [
+        (MEMBER, 'Member'),
+        (OWNER, 'Owner'),
+        (ADMIN, 'Admin'),
+        (RESTRICTED, 'Restricted'),
+        (LEFT, 'Left'),
+        (BANNED, 'Banned')
+    ]
+    participant_type = models.CharField(choices=PARTICIPANT_TYPE_CHOICES, default=MEMBER)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
 class Chat(models.Model):
     PRIVATE = 'private'
     GROUP = 'group'
@@ -21,19 +40,23 @@ class Chat(models.Model):
         (PRIVATE, 'Private'),
         (GROUP, 'Group')
     ]
+    name = models.CharField(max_length=100)
+    username = models.CharField(max_length=100)
+    description = models.CharField(default=None)
     chat_type = models.CharField(
         choices=CHAT_TYPE_CHOICES,
         default=PRIVATE
     )
-    participant_ids = ArrayField(models.IntegerField())
+    participants = models.ManyToManyField(Participant)
     updated_at = models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
 
 class Message(models.Model):
     author = models.ForeignKey(User, on_delete=models.PROTECT)
+    chat = models.ForeignKey(Chat, on_delete=models.CASCADE)
     text = models.TextField()
-    file = models.FileField(upload_to='uploads/%Y/%m/%d', blank=True)
+    attachment = models.FileField(upload_to='uploads/%Y/%m/%d', blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     reply_to = models.ForeignKey('self', on_delete=models.PROTECT, blank=True)
