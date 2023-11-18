f7fe09399e4b4718b33e8ae8a3fc170e2becbdcf
el-bekasto <beksei7@gmail.com>
2023-11-18 14:38:09 +0600

    fixed getchats view

--- backend/api/views.py
+++ backend/api/views.py
@@ -4,13 +4,17 @@ from rest_framework.request import Request
 from rest_framework.response import Response
 
 from .serializers import *
-from chat.models import Chat
+from chat.models import Chat, Participant
 
 
 class ListChats(ListAPIView):
     def get(self, request: Request, *args, **kwargs):
+        user_chats = [p.chat.pk for p in Participant.objects.filter(
+            user_id=request.user.pk,
+            participant_type__in=[Participant.MEMBER, Participant.OWNER, Participant.ADMIN]
+        )]
         result = ChatsSerializer(
-            Chat.objects.filter(participants__user_id=request.user.pk),
+            Chat.objects.filter(id__in=user_chats),
             many=True
         ).data
         return Response(result)
